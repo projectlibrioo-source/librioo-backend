f43b965c7449f1eaef9cf1faa440d85b67e5aa3c
DinukaD-24 <dinukailangakoon@gmail.com>
2026-03-17 15:22:29 +0530

    inject TransactionRepo and BookRepo into ReportsService

--- src/main/java/org/example/projectlibrioo/Service/Admin/ReportsService.java
+++ src/main/java/org/example/projectlibrioo/Service/Admin/ReportsService.java
@@ -1,11 +1,19 @@
 package org.example.projectlibrioo.Service.Admin;
 
 import org.springframework.stereotype.Service;
+import org.example.projectlibrioo.Repository.BookRepo;
+import org.example.projectlibrioo.Repository.TransactionRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import org.example.projectlibrioo.DTO.DashboardStatsDTO;
 
 @Service
 public class ReportsService {
+    @Autowired
+    private TransactionRepo transactionRepo;
+
+    @Autowired
+    private BookRepo bookRepo;
 
     public DashboardStatsDTO getDashboardStats() {
         DashboardStatsDTO stats = new DashboardStatsDTO();
